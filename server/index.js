07004ca2feb8e5e6bbf32f335db45654821b6e8f
badalraja <badal.raja@kmart.com.au>
2023-04-04 10:32:25 +1000

    assignment to prove the giftlist is in the list

--- server/index.js
+++ server/index.js
@@ -1,21 +1,31 @@
 const express = require('express');
+const MerkleTree = require('../utils/MerkleTree');
 const verifyProof = require('../utils/verifyProof');
+const niceList = require('../utils/niceList');
 
 const port = 1225;
 
 const app = express();
 app.use(express.json());
 
-// TODO: hardcode a merkle root here representing the whole nice list
-// paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const merkleTree = new MerkleTree(niceList);
+
+// get the root
+const root = merkleTree.getRoot();
+
+// find the proof that norman block is in the list 
+
+
 
 app.post('/gift', (req, res) => {
   // grab the parameters from the front-end here
   const body = req.body;
-
+  const name = body.name
+  const index = niceList.findIndex(n => n === name);
+  const proof = merkleTree.getProof(index);
+    
   // TODO: prove that a name is in the list 
-  const isInTheList = false;
+  const isInTheList = verifyProof(proof, name, root);
   if(isInTheList) {
     res.send("You got a toy robot!");
   }
